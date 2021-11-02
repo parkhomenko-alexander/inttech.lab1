ab187d15ec5b416c61d8e09e573de9107650101c
sanya <sanec_290600@mail.ru>
2021-11-02 23:24:30 +1000

    init

--- config.py
+++
@@ -1,8 +0,0 @@
-class Configuration(object):
-    DEBUG = True
-    SECRET_KEY = 'wadqwqwrqvwteltw[wr[qq'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:password@localhost/db2'
-    JWT_TOKEN_LOCATION = 'cookies'
-    JWT_COOKIE_CSRF_PROTECT = False
-    JWT_ACCESS_COOKIE_PATH = '/'
